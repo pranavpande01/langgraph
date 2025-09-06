c576b2a9c25b4bede27d7b30a30a6122fcf73645
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2025-09-06 19:43:09 +0000

    fifth commit

---
+++ st_frontend.py
@@ -0,0 +1,35 @@
+import streamlit as st
+#streamlit session state is a dict whose contents donot reset unless page is refreshed
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history']=[]
+
+
+user_input=st.chat_input("type here...")
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+if user_input:
+    st.session_state['message_history'].append(
+        {
+            'role':'user',
+            'content':user_input
+        }
+    )
+    with st.chat_message("user"):
+        st.text(user_input)
+
+
+    st.session_state['message_history'].append(
+        {
+            'role':'assistant',
+            'content':user_input
+        }
+    )
+
+    with st.chat_message("assistant"):
+        st.text(user_input)
+
